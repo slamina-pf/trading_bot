0fb94eec6c071252a219cc6fa99b3326410ef2ed
slamina.pf <slamina.pf@gmail.com>
2025-05-14 22:36:18 -0600

    Improving the helpers and starting with the execution of the trade

---
+++ app/helpers/execute_trading.py
@@ -0,0 +1,7 @@
+class ExecuteTrading:
+    def __init__(self, trading_strategy):
+        self.trading_strategy = trading_strategy
+
+    def execute(self):
+        # Implement the logic to execute the trading strategy
+        pass
